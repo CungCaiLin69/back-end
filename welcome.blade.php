3e7cd0fbec070f9aa366277e8e37fdfe71f8964e
Muhammad Sendy Adriansyah Prathama <89314291+CungCaiLin69@users.noreply.github.com>
2022-02-27 23:44:44 +0700

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/back-end.css') }}">
+    <title>Villedor Library</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Aldrich&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <h1>VILLEDOR LIBRARY</h1>
+
+		<div class="navbar">
+            <div class="menu">
+                <a class ="button"href="#view">View Book</a>
+                <a class ="button"href="#add">Add Book</a>
+                <a class ="button"href="#update">Update Book</a>
+                <a class ="button"href="#delete">Delete Book</a>
+            </div>
+        </div>
+</body>
+</html>
